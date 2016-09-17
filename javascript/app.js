f96aed8985763c7b081a91cb55933557d9aeb0c4
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-09-16 23:16:38 -0300

    Add basic service and inject it into controllers

--- javascript/app.js
+++ javascript/app.js
@@ -2,14 +2,34 @@
   'use strict'
   angular.module('ShoppingListCheckOff', [])
   .controller('ToBuyShoppingController', ToBuyShoppingController)
-  .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController);
+  .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  ToBuyShoppingController.$inject = ['$scope'];
-  function ToBuyShoppingController($scope) {
-   
+  ToBuyShoppingController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+  function ToBuyShoppingController($scope, ShoppingListCheckOffService) {
+    var toBuyCtrl = this; 
+    toBuyCtrl.toBuyItems = ShoppingListCheckOffService.toBuyItems; 
   }
-  AlreadyBoughtShoppingController.$inject = ['$scope'];
-  function AlreadyBoughtShoppingController($scope) {
-   
+  AlreadyBoughtShoppingController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+  function AlreadyBoughtShoppingController($scope, ShoppingListCheckOffService) {
+    var boughtCtrl = this;
+    boughtCtrl.boughItems = ShoppingListCheckOffService.boughItems;
+  }
+
+  function ShoppingListCheckOffService(){
+    var service = this;
+    service.toBuyItems = [
+      { name: "Cookies", quantity: 10 },
+      { name: "DC Comics", quantity: 1 },
+      { name: "Chocolate Milkshake", quantity: 1 },
+      { name: "Art Brut Album", quantity: 1 },
+      { name: "Couch", quantity: 1 },
+    ];
+    service.boughItems = [];
+
+    service.buyItem = function(itemIndex){
+      var boughtItem = service.toBuyItems.splice(itemIndex, 1);
+      service.boughItems.push(boughtItem);
+    }
   }
 })();
